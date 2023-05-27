4fdfd1dff223a4fdaaa73d3b5a3bd0279daaf776
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-05-27 23:27:08 +0530

    Assessment

--- d_s/models.py
+++ d_s/models.py
@@ -37,7 +37,7 @@ class Visitor(models.Model):
 
 class Watchman(models.Model):
 	name=models.CharField(max_length=100)
-	house=models.CharField(max_length=50)
+	house=models.ForeignKey(User,on_delete=models.CASCADE)
 	mobile=models.PositiveIntegerField()
 	complain=models.TextField()
 	date=models.DateTimeField()
